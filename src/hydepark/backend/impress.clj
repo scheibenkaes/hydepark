8bc420e67c412a9c7451c80896dd75fb9e79fe97
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-02-05 22:24:27 +0100

    first backend

--- src/hydepark/backend/impress.clj
+++ src/hydepark/backend/impress.clj
@@ -1,9 +1,25 @@
 (ns hydepark.backend.impress
-  [:use [hiccup core]]
+  [:use [hiccup page-helpers]]
   [:use [hydepark.core]])
 
-(deftype ImpressBackend []
-  PresentationBackend
-  )
+(defn render-slide [[tag attrs content]]
+  [tag (assoc attrs :class "slide") content])
 
-(def backend (ImpressBackend.))
+(defn render-html [info slides]
+  (html5
+   [:head
+    [:title (:title info)]
+    (include-js "impress.js")]
+   [:body
+    [:div.container (map render-slide slides)]]))
+
+(deftype ImpressBackend []
+  hydepark.core.PresentationBackend
+  (render [this presentation]
+    (render-html (:info presentation) (:slides presentation))))
+
+(def backend (hydepark.backend.impress.ImpressBackend.))
+
+(defn ->str []
+  (binding [*backend* backend]
+    (render *backend* @*presentation*)))
--- src/hydepark/core.clj
+++ src/hydepark/core.clj
@@ -3,7 +3,9 @@
 (def empty-presentation
   {:slides [] :info {}})
 
-(defonce ^:dynamic *presentation* (atom empty-presentation))
+(def ^:dynamic *presentation* (atom empty-presentation))
+
+(def ^:dynamic *backend* nil)
 
 (defn add-slide [slide]
   (swap! *presentation* update-in [:slides] conj slide))
@@ -13,12 +15,13 @@
 
 (defn defpresentation
   [& info]
-  (swap! *presentation* update-in [:info] merge (hash-map ~@info)))
-
-(defn defslide [& body]
-  (-> (apply slide body) add-slide))
+  (swap! *presentation* update-in [:info] merge (apply hash-map info)))
 
 (defn slide
   "Define a single slide. body must consist of key-val-pairs for slide info and a last element which is used as the content of the slide."
   [& body]
   [:div (apply hash-map (butlast body)) (last body)])
+
+(defn defslide [& body]
+  (-> (apply slide body) add-slide))
+
